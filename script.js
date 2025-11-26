821b15ce2890410c2d33e2c50bc47ff3e87e001a
Supravisor <153783117+Supravisor@users.noreply.github.com>
2025-11-27 11:22:02 +1300

    remove checkbox from edit daily task

--- script.js
+++ script.js
@@ -300,9 +300,9 @@ let diff = new Date(thisYear, thisMonth, thisDay).getTime();
     adHocList.classList.remove("hidden");
 
     // ad hoc
-    adHocTitle.innerHTML = `<h2>${new Date().toLocaleString("default", { weekday: "long" })} ${new Date().getDate()}</h2>`;
+    adHocTitle.innerHTML = `<h2 class="short-daily">${new Date().toLocaleString("default", { weekday: "short" })} ${new Date().getDate()}</h2>`;
+    adHocTitle.innerHTML += `<h2 class="long-daily">${new Date().toLocaleString("default", { weekday: "long" })} ${new Date().getDate()}</h2>`;
     adHocList.innerHTML = tasks["Daily"]["Ad hoc"].map(el => `
-      <input type="checkbox" />
       <li>
         <label>
           ${el}
